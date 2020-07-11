29242c16e497f4c45fc1ad345fb6560ed11877cc
Omar Sharaki <omarsharaki@gmail.com>
2020-07-11 20:48:22 +0200

    :white_check_mark: Updated parser tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,6 +1,20 @@
 import parseEntries from "../src/scripts/parser";
 
-test("Entry parsing", () => {
-    let result = parseEntries("banana 50");
-    expect(result).toStrictEqual([{ amount: "50", item: "banana" }]);
+describe('Regex matching', () => {
+    test("Entry parsing", () => {
+        let result = parseEntries("banana 50");
+        expect(result).toStrictEqual([{ amount: "50", item: "banana" }]);
+
+        result = parseEntries("milch (3.5%) 50 milch (3.5%) 50");
+        expect(result).toStrictEqual([{ amount: "50", item: "milch (3.5%) 50 milch (3.5%)" }]);
+
+        result = parseEntries("milch 3.5% 50");
+        expect(result).toStrictEqual([{ amount: "50", item: "milch 3.5%" }]);
+
+        result = parseEntries("1.5% milch (3.5%) 50");
+        expect(result).toStrictEqual([{ amount: "50", item: "milch (3.5%)" }]);
+
+        result = parseEntries("müsli 50");
+        expect(result).toStrictEqual([{ amount: "50", item: "müsli" }]);
+    });
 });
