06aba8306b634db910f1d0416657daf4693b1923
Omar Sharaki <omarsharaki@gmail.com>
2020-07-12 15:25:50 +0200

    :white_check_mark: Updated tests

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,20 +1,82 @@
-import parseEntries from "../src/scripts/parser";
+import { parseEntries, removeWrappingSpaces } from "../src/scripts/parser";
 
 describe('Regex matching', () => {
-    test("Entry parsing", () => {
-        let result = parseEntries("banana 50");
-        expect(result).toStrictEqual([{ amount: "50", item: "banana" }]);
+    describe("Single line", () => {
+        test("Standard", () => {
+            let result = parseEntries("milch (3.5%) 50 milch (3.5%) 50", true);
+            expect(result).toStrictEqual([{ amount: "50", item: "milch (3.5%) 50 milch (3.5%)" }]);
 
-        result = parseEntries("milch (3.5%) 50 milch (3.5%) 50");
-        expect(result).toStrictEqual([{ amount: "50", item: "milch (3.5%) 50 milch (3.5%)" }]);
+            result = parseEntries("müsli milch (3.5%)          50  ", true);
+            expect(result).toStrictEqual([{ amount: "50", item: "müsli milch (3.5%)" }]);
 
-        result = parseEntries("milch 3.5% 50");
-        expect(result).toStrictEqual([{ amount: "50", item: "milch 3.5%" }]);
+            result = parseEntries("50   milch (3.5%) 50 milch (3.5%) ", false);
+            expect(result).toStrictEqual([{ amount: "50", item: "milch (3.5%) 50 milch (3.5%)" }]);
 
-        result = parseEntries("1.5% milch (3.5%) 50");
-        expect(result).toStrictEqual([{ amount: "50", item: "milch (3.5%)" }]);
+            result = parseEntries("50    müsli milch (3.5%)          ", false);
+            expect(result).toStrictEqual([{ amount: "50", item: "müsli milch (3.5%)" }]);
+        });
 
-        result = parseEntries("müsli 50");
-        expect(result).toStrictEqual([{ amount: "50", item: "müsli" }]);
+        test("Fractions", () => {
+            result = parseEntries("    müsli milch (3.5%)          0.5  ", true);
+            expect(result).toStrictEqual([{ amount: "0.5", item: "müsli milch (3.5%)" }]);
+
+            let result = parseEntries("0.5    müsli milch (3.5%)          ", false);
+            expect(result).toStrictEqual([{ amount: "0.5", item: "müsli milch (3.5%)" }]);
+        });
     });
+
+    describe("Multi line", () => {
+        test("Standard", () => {
+            let result = parseEntries(`banana 50  
+            müsli milch (3.5%) 150`
+                , true);
+            expect(result).toStrictEqual([
+                { amount: "50", item: "banana" },
+                { amount: "150", item: "müsli milch (3.5%)" }
+            ]);
+
+            result = parseEntries(`   50 banana   
+            150 müsli milch (3.5%) `
+                , false);
+            expect(result).toStrictEqual([
+                { amount: "50", item: "banana" },
+                { amount: "150", item: "müsli milch (3.5%)" }
+            ]);
+        })
+
+        describe("Inconsistent ordering", () => {
+            test("Item amount", () => {
+                let result = parseEntries(`   50 banana   
+                hard boiled eggs 150`
+                    , true);
+                expect(result).toStrictEqual([
+                    { amount: "150", item: "hard boiled eggs" },
+                    null
+                ]);
+            })
+
+            test("Amount item", () => {
+                let result = parseEntries(`   50 banana   
+                           hard boiled eggs 150   `
+                    , false);
+                expect(result).toStrictEqual([
+                    { amount: "50", item: "banana" },
+                    null
+                ]);
+            });
+        });
+    });
+
+    // test("With unit", () => {
+    //     let result = parseEntries(`banana [cup] 1
+    //     hard boiled eggs 1`);
+    //     expect(result).toStrictEqual([{ amount: "1", item: "banana" }]);
+    // });
+});
+
+
+
+test("Wrapping spaces", () => {
+    let result = removeWrappingSpaces("       müsli milch (3.5%)    ");
+    expect(result).toBe("müsli milch (3.5%)");
 });
