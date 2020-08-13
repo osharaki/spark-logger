02adfca437992edcd7b7ce977eff1e57e1a6f8ed
Omar Sharaki <omarsharaki@gmail.com>
2020-08-13 14:25:39 +0200

    :truck: Moved background.js to root

--- background.js
+++ background.js
@@ -1,16 +1,17 @@
 // Fired when the extension is first installed, when the extension is updated to a new version, and when Chrome is updated to a new version
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ color: '#3aa757' },
-        () => console.log("The color is green"));
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
         console.log('removed rules');
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostContains: 'sparkpeople.com' },
+                pageUrl: { urlMatches: '.*' },
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }], () => console.log('added rules'));
     });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 // TODO: remove console logs
