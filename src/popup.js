e8889ba3feeeaa67a086778a0890d0c99f988d08
Omar Sharaki <omarsharaki@gmail.com>
2020-08-14 20:46:47 +0200

    :construction: Parsed entries being received from parser.js and popup.html dynamically altered accordingly

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,25 @@
-let textArea = document.getElementById("textarea");
+const div_main = document.getElementById("containerMain");
+const textArea = document.getElementById("textarea");
+const div_parsedEntries = document.createElement('div');
+div_parsedEntries.setAttribute("style", "display: inline-block");
+div_main.appendChild(div_parsedEntries);
 
 textArea.oninput = () => {
+    div_parsedEntries.innerHTML = '';
     const content = textArea.value;
     console.log(content);
-    chrome.runtime.sendMessage({ msg: "User Input", data: content });
+    chrome.runtime.sendMessage({ msg: "User Input", data: content }, (response) => {
+        if (response) {
+            if (response.data) {
+                for (const entry of response.data) {
+                    console.log(entry);
+                    const p_parsedEntry = document.createElement('p');
+                    const p_parsedEntryText = document.createTextNode(`${entry.item} ${entry.amount}`);
+                    p_parsedEntry.appendChild(p_parsedEntryText);
+                    div_parsedEntries.appendChild(p_parsedEntry);
+                }
+                // document.body.appendChild(div_parsedEntries);
+            }
+        }
+    });
 };
