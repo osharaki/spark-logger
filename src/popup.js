fbf11dea708ac1346524cc4ccdfa63f3d2eea819
Omar Sharaki <omarsharaki@gmail.com>
2020-08-17 08:20:29 +0200

    :lipstick: Textarea now expands verically when user types

--- src/popup.js
+++ src/popup.js
@@ -10,6 +10,11 @@ tr_headers.appendChild(th_amount);
 const textArea = document.getElementById("textarea");
 
 textArea.oninput = () => {
+    // https://stackoverflow.com/questions/7745741/auto-expanding-textarea
+    var heightLimit = 300; /* Maximum height: 200px */
+    textarea.style.height = ""; /* Reset the height*/
+    textarea.style.height = Math.min(textarea.scrollHeight, heightLimit)+2 + "px"; // the 2 is added to account for the top and bottom borders (1px each)
+
     table_parsedEntries.innerHTML = '';
     table_parsedEntries.appendChild(tr_headers);
     const content = textArea.value;
@@ -18,15 +23,17 @@ textArea.oninput = () => {
         if (response) {
             if (response.data) {
                 for (const entry of response.data) {
-                    console.log(entry);
-                    const tr_parsedEntry = document.createElement('tr');
-                    const td_parsedItem = document.createElement('td');
-                    td_parsedItem.appendChild(document.createTextNode(`${entry.item}`));
-                    const td_parsedAmount = document.createElement('td');
-                    td_parsedAmount.appendChild(document.createTextNode(`${entry.amount}`));
-                    tr_parsedEntry.appendChild(td_parsedItem);
-                    tr_parsedEntry.appendChild(td_parsedAmount);
-                    table_parsedEntries.appendChild(tr_parsedEntry);
+                    if (entry) {
+                        console.log(entry);
+                        const tr_parsedEntry = document.createElement('tr');
+                        const td_parsedItem = document.createElement('td');
+                        td_parsedItem.appendChild(document.createTextNode(`${entry.item}`));
+                        const td_parsedAmount = document.createElement('td');
+                        td_parsedAmount.appendChild(document.createTextNode(`${entry.amount}`));
+                        tr_parsedEntry.appendChild(td_parsedItem);
+                        tr_parsedEntry.appendChild(td_parsedAmount);
+                        table_parsedEntries.appendChild(tr_parsedEntry);
+                    }
                 }
             }
         }
