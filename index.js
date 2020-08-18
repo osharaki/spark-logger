b41650116ec1468f6eb5828336dde91d12264e33
Omar Sharaki <omarsharaki@gmail.com>
2020-08-18 20:20:43 +0200

    :truck: Moved all code to /src

--- index.js
+++
@@ -1,8 +0,0 @@
-import { parseEntries } from "./src/scripts/parser.js";
-import { findFavs, fillFavs } from "./src/scripts/crawler";
-
-function foodLogger(entries){
-    const entries = parseEntries(entries);
-    const elements = findFavs(entries);
-    fillFavs(elements);
-}
