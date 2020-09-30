890b4d1f2e64e14e38e251a3c3dcca50bea0f394
Omar Sharaki <omarsharaki@gmail.com>
2020-09-30 10:43:51 +0200

    :recycle: Refactored parser

--- src/scripts/parser.js
+++ src/scripts/parser.js
@@ -10,18 +10,21 @@ function parseEntries(entries, isItemAmount = true) {
             for (const line of lines) {
                 const matches = line.match(/^ *[0-9]+(.[0-9]+)? (?=(.*$))/m); // global flag removed to be able to access capturing groups
                 if (matches)
-                    matches[2] ? entriesParsed.push({ amount: removeWrappingSpaces(matches[0]), item: removeWrappingSpaces(matches[2]) })
-                        : entriesParsed.push({ amount: removeWrappingSpaces(matches[0]), item: null });
+                    entriesParsed.push({ amount: removeWrappingSpaces(matches[0]), item: removeWrappingSpaces(matches[2]) })
+
+                /* An alternative that can be used for targeted parse warnings */
+                // matches ? entriesParsed.push({ amount: removeWrappingSpaces(matches[0]), item: removeWrappingSpaces(matches[2]) }) : entriesParsed.push(null);
             }
         }
         else {
             // item followed by amount
             for (const line of lines) {
                 const matches = line.match(/^.* (?=([0-9]+(.[0-9]+)? *$))/m);
-                // const matches = line.match(/^.* |[0-9]+ *$/gm);
                 if (matches)
-                    matches[1] ? entriesParsed.push({ amount: removeWrappingSpaces(matches[1]), item: removeWrappingSpaces(matches[0]) })
-                        : entriesParsed.push({ amount: null, item: removeWrappingSpaces(matches[0]) });
+                    entriesParsed.push({ amount: removeWrappingSpaces(matches[1]), item: removeWrappingSpaces(matches[0]) })
+
+                /* An alternative that can be used for targeted parse warnings */
+                // matches ? entriesParsed.push({ amount: removeWrappingSpaces(matches[1]), item: removeWrappingSpaces(matches[0]) }) : entriesParsed.push(null);
             }
         }
         // This null element is used for warning in the UI
@@ -29,7 +32,6 @@ function parseEntries(entries, isItemAmount = true) {
             entriesParsed.push(null);
     }
 
-    // console.log(entriesParsed);
     return entriesParsed;
 }
 
